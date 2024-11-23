9238c41c0facbdc817e4788bf7fe5dcbd47453fa
noslobots <fran.montoiro@gmail.com>
2024-11-23 13:02:49 +0100

    Added parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallax;
+    Material mat;
+    Transform cam;
+    Vector3 initialPos;
+
+    void Start()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+        
+        cam = Camera.main.transform;
+
+        initialPos = transform.position;
+    }
+
+    
+    void Update()
+    {
+        // la capa de parallax se posiciona en X con la cámara (Y, Z se mantienen a su posición inicial)
+        transform.position = new Vector3(cam.position.x, initialPos.y, initialPos.z);
+
+        // la textura de la capa de parallax se desplaza en X en función del desplazamiento de la cámara
+        mat.mainTextureOffset = new Vector2(cam.position.x * parallax, 0);
+    }
+}
